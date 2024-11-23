4ef1d662af84d319454cdbc2b9c40e5fb872dcdd
Le-Jr <nascimentoleandrojr@gmail.com>
2024-11-23 20:29:42 -0300

    Finish Main Style

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,35 +1,50 @@
-import { H1, H2, P, TitleStyle } from "./style";
+import {
+  BigCup,
+  Button,
+  Div,
+  H1,
+  H2,
+  ImgCup,
+  ImgElipse,
+  Main,
+  P,
+  TitleStyle,
+} from "./style";
 
 const Home = () => {
   return (
-    <main>
-      <TitleStyle className="Title">
-        <H2>Mais que Café</H2>
-        <H1>
-          Isso é <span className="starbucks">Starbucks</span>
-        </H1>
+    <>
+      <Main>
+        <TitleStyle className="Title">
+          <H2>Mais que Café</H2>
+          <H1>
+            Isso é <span className="starbucks">Starbucks</span>
+          </H1>
 
-        <P>
-          A Starbucks oferece uma variedade de cafés de alta qualidade. Alguns
-          dos cafés mais populares incluem o Caffè Americano, o Cappuccino, o
-          Latte Macchiato e o Espresso. Além disso, a Starbucks oferece bebidas
-          quentes e frias, doces diferenciados e sanduíches.
-        </P>
+          <P>
+            A Starbucks oferece uma variedade de cafés de alta qualidade. Alguns
+            dos cafés mais populares incluem o Caffè Americano, o Cappuccino, o
+            Latte Macchiato e o Espresso. Além disso, a Starbucks oferece
+            bebidas quentes e frias, doces diferenciados e sanduíches.
+          </P>
 
-        <button>Saiba Mais</button>
+          <Button>SAIBA MAIS</Button>
+        </TitleStyle>
 
-        <div>
+        <BigCup className="BigCup">
+          <ImgCup src="copo_grande_amarelo.png" alt="" />
+          <ImgElipse src="elipse_verde.png" alt="" />
+        </BigCup>
+      </Main>
+
+      <footer>
+        <Div>
           <img src="copo_pequeno_amarelo.png" alt="" />
           <img src="copo_pequeno_vermelho.png" alt="" />
           <img src="copo_pequeno_laranja.png" alt="" />
-        </div>
-      </TitleStyle>
-
-      <section className="BigCup">
-        <img src="" alt="" />
-        <div className="circle"></div>
-      </section>
-    </main>
+        </Div>
+      </footer>
+    </>
   );
 };
 export default Home;
--- src/Components/Home/style.js
+++ src/Components/Home/style.js
@@ -1,14 +1,21 @@
 import { styled } from "styled-components";
 
+export const Main = styled.main`
+  display: flex;
+  justify-content: space-between;
+`;
+
 export const TitleStyle = styled.section`
   font-family: "Poppins", sans-serif;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  justify-content: center;
-  height: 100vh;
+  justify-content: flex-start;
+  height: 60vh;
   width: 800px;
   margin-left: 5em;
+  margin-top: 6em;
+  /* border: solid 1px black; */
 `;
 
 export const H1 = styled.h1`
@@ -16,9 +23,10 @@ export const H1 = styled.h1`
   margin-bottom: 20px;
   font-weight: 400;
 
-  span {
+  span.starbucks {
     color: #037143;
     font-weight: 700;
+    font-size: 96px;
   }
 `;
 
@@ -30,8 +38,55 @@ export const H2 = styled.h2`
 `;
 
 export const P = styled.p`
+  width: 700px;
   font-size: 24px;
   margin-bottom: 20px;
   font-weight: 400;
   line-height: 36px;
+`;
+
+export const Button = styled.button`
+  font-family: "Inter", sans-serif;
+  font-weight: 700;
+  background-color: #037143;
+  color: #fff;
+  border: none;
+  border-radius: 35px;
+  padding: 15px 60px;
+  font-size: 20px;
+  cursor: pointer;
+`;
+
+export const Div = styled.div`
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  gap: 50px;
+  /* border: solid 1px black; */
+
+  img:hover {
+    transform: rotate(12deg);
+    cursor: pointer;
+  }
+`;
+
+export const BigCup = styled.section`
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  /* border: solid 1px black; */
+  position: relative;
+  z-index: 2;
+`;
+
+export const ImgElipse = styled.img`
+  position: absolute;
+  right: 0;
+  top: 50%;
+  transform: translateY(-8%);
+  z-index: 1;
+`;
+
+export const ImgCup = styled.img`
+  z-index: 2;
 `;
