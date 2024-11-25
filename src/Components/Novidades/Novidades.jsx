f89887ddb58c23998f98020409f4173136841465
Le-Jr <nascimentoleandrojr@gmail.com>
2024-11-24 22:06:21 -0300

    Finish routes

--- src/Components/Novidades/Novidades.jsx
+++ src/Components/Novidades/Novidades.jsx
@@ -1,8 +1,22 @@
+import { Button, H2, Img, P, Section, StyledP, TextDiv } from "./style";
+
 const Novidades = () => {
   return (
-    <section>
-      <h2>Novidades</h2>
-    </section>
+    <Section>
+      <TextDiv>
+        <StyledP>PREPARAÇÃO</StyledP>
+        <H2>Níveis de torra</H2>
+        <P>
+          Qual a torra que prefere? Starbucks® Torra Clara, Torra Média ou Torra
+          Escura? Estas sãos as torras que fazem parte dos níveis de torra
+          Starbucks®
+        </P>
+        <Button>SAIBA MAIS</Button>
+      </TextDiv>
+      <div>
+        <Img src="grao-cafe.png" alt="" />
+      </div>
+    </Section>
   );
 };
 export default Novidades;
--- src/Components/Sobre/Sobre.jsx
+++ src/Components/Sobre/Sobre.jsx
@@ -1,8 +1,22 @@
+import { Button, H2, Img, P, Section, StyledP, TextDiv } from "./style";
+
 const Sobre = () => {
   return (
-    <section>
-      <h2>Sobre</h2>
-    </section>
+    <Section>
+      <div>
+        <Img src="foto-starbucks.png" alt="" />
+      </div>
+      <TextDiv>
+        <StyledP>PREPARAÇÃO</StyledP>
+        <H2>Níveis de torra</H2>
+        <P>
+          Qual a torra que prefere? Starbucks® Torra Clara, Torra Média ou Torra
+          Escura? Estas sãos as torras que fazem parte dos níveis de torra
+          Starbucks®
+        </P>
+        <Button>SAIBA MAIS</Button>
+      </TextDiv>
+    </Section>
   );
 };
 export default Sobre;
